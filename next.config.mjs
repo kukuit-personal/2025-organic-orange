ffa0c770a973773504b184c3b65797e1d0818467
Khang Huynh <khangover27@gmail.com>
2026-01-12 22:38:16 +0700

    change-end3

--- next.config.mjs
+++ next.config.mjs
@@ -17,10 +17,6 @@ const nextConfig = {
         source: '/icon-48-v2.png',
         headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
       },
-      {
-        source: '/favicon-v2.ico',
-        headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
-      },
     ]
   },
 }
